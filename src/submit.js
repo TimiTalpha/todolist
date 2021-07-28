d6147a4f77996ce14be69b6742a8deb618d47b3a
TimiTalpha <s1290237@u-aizu.ac.jp>
2021-07-28 19:03:15 +0900

    second commit

--- src/submit.js
+++ src/submit.js
@@ -1,16 +1,19 @@
 const input = document.getElementById('input');
 const Add = document.getElementById('button');
 const list = document.getElementById('todo-list');
+let id = 0;
 
 Add.addEventListener('click', () => {
    const li = document.createElement('li');
    const button = document.createElement('input');
    const label = document.createElement('label');
    
+   button.id = String(id);
    label.textContent = input.value;
    button.type = 'button';
    button.value = "remove";
    button.addEventListener('click', () => {
+         localStorage.removeItem(button.id);
      li.remove();
    })
 
@@ -18,9 +21,13 @@ Add.addEventListener('click', () => {
    li.appendChild(button);
 
    list.appendChild(li);
-
+   localStorage.setItem(id, input.value);
+   
    input.value = "";
-
+   id++;
+   
+   console.log(localStorage);
 
 })
+
 
